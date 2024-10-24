d536c48dc40ec8b809642ab55c678ea8f2dc2957
ilyxa <iluxa@ilyxas-MacBook-Pro.local>
2024-10-25 01:33:42 +0300

    misspell-fix

--- components/Random.vue
+++ components/Random.vue
@@ -2,9 +2,17 @@
 import { ref } from 'vue'
 
 const { random } = useAvailableCountries()
+const { getHoliday, nextHoliday } = useNextPublicHolidays()
 const randomData = ref(random)
+const randomHoliday = ref([])
+
+for (let i = 0; i < randomData.value.length; i++) {
+    console.log(randomData.value[i])
+}
 </script>
 
 <template>
-    <div></div>
+    <div>
+        {{ randomData }}
+    </div>
 </template>
--- composables/useAvailableCountries.ts
+++ composables/useAvailableCountries.ts
@@ -2,33 +2,27 @@ import { useRuntimeConfig } from 'nuxt/app'
 import { ref } from 'vue'
 
 export const useAvailableCountries = () => {
-    const countrys = ref()
     const config = useRuntimeConfig()
+    const countries = ref()
     const random = ref([])
 
-    const countrysData = () => {
-        fetch(config.public.api_root, {
-            method: 'GET',
+    fetch(config.public.api_root, {
+        method: 'GET',
+    })
+        .then((res) => res.json())
+        .then(async (data) => {
+            countries.value = await data
+
+            for (let i = 0; i < 3; i++) {
+                random.value.push(
+                    countries.value[Math.floor(Math.random() * (113 - 0 + 1))],
+                )
+            }
         })
-            .then((res) => res.json())
-            .then((data) => {
-                countrys.value = data
+        .catch((err) => console.log(err.message))
 
-                for (let i = 0; i < 3; i++) {
-                    random.value.push(
-                        countrys.value[
-                            Math.floor(Math.random() * (113 - 0 + 1))
-                        ],
-                    )
-                }
-            })
-            .catch((err) => console.log(err.message))
-    }
-
-    const
-    countrysData()
     return {
-        countrys,
+        countries,
         random,
     }
 }
